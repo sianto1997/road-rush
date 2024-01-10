1656c347417f63b723033086abf5e02441a1073c
Esmeedr <esmeederoo@upcmail.nl>
2024-01-10 15:32:35 +0100

    gk

--- code/classes/Board.py
+++ code/classes/Board.py
@@ -1,6 +1,27 @@
+import matplotlib.pyplot as plt 
+
 class Board:
-    def __init__(self, size, exit_row)
+    def __init__(self, size):
         self.size = size
-        self.exit_row = exit_row
+        self.exit_row = self.size // 2
         self.cars = []
+
+    # def add_cars():
+    #     self.cars.append(Car())
+    
+    def visualize(self):
+        board = plt.figure(figsize=[6,6])
+        board.patch.set_facecolor((1,1,.8))
+        ax = board.add_subplot(111)
+        for x in range(13):
+            ax.plot([x, x], [ 0,12], 'k')
+        for y in range(12):
+            ax.plot([0, 12], [y,y], 'k')
+        ax.set_position([0,0,1,1])
+        ax.set_axis_off()
+        ax.set_xlim(-1,13)
+        ax.set_ylim(-1,13)
+        plt.savefig(
+        
+
         
---
+++ code/classes/Main.py
@@ -0,0 +1,26 @@
+import argparse
+import pandas as pd
+from Board import Board
+
+
+def main(input_file):
+    csv = pd.read_csv(input_file) 
+    
+    board = Board(6) 
+
+    board.visualize()
+
+    
+    
+if __name__ == "__main__":
+    # Set-up parsing command line arguments
+    parser = argparse.ArgumentParser(description = "reads in csv file")
+
+    # Adding arguments
+    parser.add_argument("input", help = "input file (csv)")
+
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    # Run main with provide arguments
+    main(args.input)
