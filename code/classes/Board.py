d9bada84168aa0ac15607ac2409c5251ef9e44ef
Esmeedr <esmeederoo@upcmail.nl>
2024-01-11 12:46:56 +0100

    comments

--- code/classes/Board.py
+++ code/classes/Board.py
@@ -19,17 +19,41 @@ class Board:
             self.cars.append(car)
     
     def visualize(self):
+        """
+        Visualizes the object board
+        """
+        # give the range to the figure
         board = plt.figure(figsize=[self.size+0.5,self.size+0.5])
+
+        # makes the background colour of the figure gray
         board.patch.set_facecolor('gray')
+
+        # creates the grid place
         ax = board.add_subplot()
+
+        # inverts the values of the y axis
         ax.invert_yaxis()
+
+        # loops over the values of the x
         for x in range(1,self.size+1):
+            
+            # makes a black line on the places of the x in a particular range 
             ax.plot([x, x], [1,self.size+1], 'k')
+        
+        # makes the exit row line
         ax.plot([self.size+1, self.size+1], [1,self.exit_row], 'k')
         ax.plot([self.size+1, self.size+1], [self.exit_row+1,self.size+1], 'k')
+
+        # loops over the values of y 
         for y in range(1,self.size+2):
+
+            # makes a black line on the places of the y in a particular range
             ax.plot([1, self.size+1], [y,y], 'k')
+        
+        # create marges around the grid 
         ax.set_position([0,0,1,1])
+
+        # deletes the axis numbers 
         ax.set_axis_off()
       
         plt.show()
