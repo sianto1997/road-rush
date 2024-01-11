29cabfa32d16bc0940e85c481127ac819c4b619f
Esmeedr <esmeederoo@upcmail.nl>
2024-01-11 12:20:40 +0100

    hoi

--- code/classes/Board.py
+++ code/classes/Board.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from Car import Car 
+from math import ceil
 
 class Board:
     def __init__(self, input_file):
         start = input_file.find('hour') + len('hour')
         end = input_file.find('x', start)
         self.size = int(input_file[start:end].strip())
-        self.exit_row = self.size // 2
+        self.exit_row = ceil(self.size / 2)
         self.cars = []
         print(self.size, self.exit_row)
 
@@ -18,18 +19,21 @@ class Board:
             self.cars.append(car)
     
     def visualize(self):
-        board = plt.figure(figsize=[6,6])
+        board = plt.figure(figsize=[self.size+0.5,self.size+0.5])
         board.patch.set_facecolor((1,1,.8))
-        ax = board.add_subplot(111)
-        for x in range(13):
-            ax.plot([x, x], [0,12], 'k')
-        for y in range(12):
-            ax.plot([0, 12], [y,y], 'k')
+        ax = board.add_subplot()
+        ax.invert_yaxis()
+        for x in range(1,self.size+1):
+            ax.plot([x, x], [1,self.size+1], 'k')
+        ax.plot([self.size+1, self.size+1], [1,self.exit_row], 'k')
+        ax.plot([self.size+1, self.size+1], [self.exit_row+1,self.size+1], 'k')
+        for y in range(1,self.size+2):
+            ax.plot([1, self.size+1], [y,y], 'k')
         ax.set_position([0,0,1,1])
         ax.set_axis_off()
-        ax.set_xlim(-1,13)
-        ax.set_ylim(-1,13)
-        plt.savefig(board)
+      
+        plt.show()
+
         
 
         
