657cf9ee2c5c87137f657b1eb340c228634ea3e8
Esmeedr <esmeederoo@upcmail.nl>
2024-01-12 15:54:42 +0100

    update

--- code/classes/main.py
+++ code/classes/main.py
@@ -12,7 +12,7 @@ def main(input_file):
 
     board.draw() 
 
-    experiment = Experiment(board, 10 ** 5, input_file)
+    experiment = Experiment(board, 10, input_file)
     experiment.start_random_experiment()
 
     time.sleep(2)
