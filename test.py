e5765840fb69b8288f88221f69a8e0569f738806
Esmeedr <esmeederoo@upcmail.nl>
2024-01-17 11:50:37 +0100

    bjk

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ import pandas as pd
 
 df = pd.read_csv('random_experiments.csv')
 
-df.
+df_sorted = df.sort_values(by='move', ascending=False)
 
 plt.figure(figsize=(20,10))
 
 # makes the plot with the information from the dataframe
-plot = sns.barplot(data = df, x=df.index ,y='move')
+plot = sns.barplot(data = df_sorted, x=df.index ,y='move')
 
 # makes sure the labels are at an angle and that they are readable
 num_bars = len(df)
@@ -20,6 +20,9 @@ plot.set_xticklabels(x_ticks)
 
 plot.set_xticklabels(plot.get_xticklabels(), rotation=45, ha="right")
 plt.tight_layout()
+plt.xlabel("Amount of experiments")
+plt.ylabel("Made moves")
+plt.title("Random Experiment results")
 
 # saves the outcome of the plot into a png file
 plt.savefig('graph-sorted.png')
