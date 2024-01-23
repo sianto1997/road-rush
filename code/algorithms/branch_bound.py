f5c2601c9128a46e8918fad889326383db3c88fa
Esmeedr <esmeederoo@upcmail.nl>
2024-01-23 12:24:51 +0100

    begin algoritme

--- code/algorithms/branch_bound.py
+++ code/algorithms/branch_bound.py
@@ -2,8 +2,20 @@ from typing import Protocol
 import copy
 
 class BranchAndBound(Protocol):
-    def __init__(self, board, graph, states):
-        pass
+    def __init__(self, board):
+        self.board = copy.deepcopy(board)
+
+        self.states = [copy.deepcopy(self.board)]
+    
+    def get_next_state(self):
+        return self.states.pop()
+
+    def get_children(self):
+        child = self.board.get_moves(True)
+
+        
+
+
     def run(self):
         pass
     def get_name(self):
