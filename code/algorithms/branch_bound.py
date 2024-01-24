b4d238cd26f279f33b1c2d32f753ab64c64a3560
Esmeedr <esmeederoo@upcmail.nl>
2024-01-24 12:00:02 +0100

    fdjs

--- code/algorithms/branch_bound.py
+++ code/algorithms/branch_bound.py
@@ -1,20 +1,26 @@
 from typing import Protocol
 import copy
+import 
 
 class BranchAndBound(Protocol):
     def __init__(self, board):
         self.board = copy.deepcopy(board)
 
         self.states = [copy.deepcopy(self.board)]
+
+        self.best_solution = None
     
     def get_next_state(self):
         return self.states.pop()
 
     def get_children(self):
-        child = self.board.get_moves(True)
 
-        
+        # receives the list of all the next possible nodes 
+        childs = self.board.get_moves(True)
 
+        for child in childs:
+            new_board = copy.deepcopy(child)
+            self.states.append(new_board)
 
     def run(self):
         pass
