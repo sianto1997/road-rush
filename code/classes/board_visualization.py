2cbdb525f589ffb0546fa37ae24653c7e1ebf423
Esmeedr <esmeederoo@upcmail.nl>
2024-01-30 12:28:15 +0100

    board_visualize comments

--- code/classes/board_visualization.py
+++ code/classes/board_visualization.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 
 class BoardVisualization():
     '''
-    This class manages the visualization of the experiments. It exists because a lot of different boards (states) are assessed.
-    The running experiment can replace the board that is displayed.
+    This class manages the visualization of the experiments
 
-    Input:
-    - draw_interval (float): The default value for the draw_interval. See draw function for more info.
+    Attributes
+    ----------
+    initialized : Boolean
+        Starts at True to make the figure, then is set to False so only one figures is made
+    draw_interval : float
+        The default value for the draw_interval. See draw function for more info
     '''
 
     def __init__(self, draw_interval):
@@ -16,46 +19,34 @@ class BoardVisualization():
 
     def init_visualization(self):
         '''
-        Initializes the board so that only one display is created.
+        Initializes the board so that only one display is created
         '''
-        # Give the range to the figure
         self.canvas = plt.figure(figsize = [self.board.size + 0.5, self.board.size + 0.5])
-
-        # Makes the background colour of the figure gray
         self.canvas.patch.set_facecolor('gray')
-
-        # Creates the grid place
-        self.ax = self.canvas.add_subplot()
-
-        # Give the range to the figure
-        self.canvas = plt.figure(figsize = [self.board.size + 0.5, self.board.size + 0.5])
-
-        # Makes the background colour of the figure gray
-        self.canvas.patch.set_facecolor('gray')
-
-        # Creates the grid place
         self.ax = self.canvas.add_subplot()
         self.initialized = True
     
     def replace(self, board):
         '''
-        Replace the existing board with a new one. Used for examining child states and new experiments.
+        Replace the existing board with a new one. Used for examining child states and new experiments
         
-        Input:
-        - board (Board): The new board
+        Parameter
+        ---------
+        - board : Board
+            The new board
         '''
         self.board = board
 
-        # Initialize (only if not already initialized)
         if not self.initialized:
             self.init_visualization()
 
     def draw(self, draw_interval = 0):
         '''
-        Draws the object board to the current state.
+        Draws the object board to the current state
     
-        Input:
-        - draw_interval (float): A number between 0.0001 and 1000.0. This float is for showing a part of the simulation longer. By default, it uses the configured draw_interval.
+        Parameter
+        - draw_interval : float
+            A number between 0.0001 and 1000.0. shows a part of the simulation longer (default is 0)
         '''
         if not self.initialized:
             return
@@ -63,63 +54,49 @@ class BoardVisualization():
         if draw_interval == 0:
             draw_interval = self.draw_interval
         
-        # Inverts the values of the y-axis
         self.ax.invert_yaxis()
         
-        # Loops over the values of the x-axis
         for x in range(1, self.board.size + 1):
-            # Makes a black line on the places of the x in a particular range 
             self.ax.plot([x, x], [1, self.board.size + 1], 'k')
         
-        # Makes the exit row line
         self.ax.plot([self.board.size + 1, self.board.size + 1], [1, self.board.exit_row], 'k')
         self.ax.plot([self.board.size + 1, self.board.size + 1], [self.board.exit_row + 1,self.board.size + 1], 'k')
 
-        # Loops over the values of y-axis
         for y in range(1, self.board.size + 2):
-
-            # Makes a black line on the places of the y in a particular range
             self.ax.plot([1, self.board.size + 1], [y, y], 'k')
         
-        # Create marges around the grid 
         self.ax.set_position([0, 0, 1, 1])
-
-        # Deletes the axis numbers 
+ 
         self.ax.set_axis_off()
 
         for car in self.board.cars:
             self.draw_car(self.board.cars[car])
       
         plt.draw()
-        # Pause to be able to show the visualization
         plt.pause(self.draw_interval)
 
         self.ax.cla()
 
     def close(self):
         '''
-        Closes the visualization.
+        Closes the visualization
         '''
         plt.close(self.canvas)
 
     def draw_car(self, car):
         '''
-        Creates a simple representation of a car on an existing canvas.
+        Creates a simple representation of a car on an existing canvas
 
-        Input:
-        - car (Car): The car to draw.
-        '''
-
-        # Checks the orientation of the car object       
-        if car.orientation == 'H':
-            # Creates a rectangle in horizontal oriantion with the parameters of the object car 
+        Parameter
+        ---------
+        car : Car
+            The car to draw
+        '''      
+        if car.orientation == 'H': 
             car_drawing = plt.Rectangle((car.column, car.row), car.length, 1, facecolor = car.colour, edgecolor = 'black', lw = 5)
         else:
-            # Creates a rectangle in vertical oriantion with the parameters of the object car
             car_drawing = plt.Rectangle((car.column, car.row), 1, car.length, facecolor = car.colour, edgecolor = 'black', lw = 5)
         
-        # Adds the made rectangles to the board
         self.ax.add_patch(car_drawing)
 
-        # Shows the id of the car in the plot 
         plt.text(car.column + 0.5, car.row + 0.5, car.id, fontsize = 10)
