a9f08d34e9a4eca349c577b2ac3646bf8e979577
Amber Korte <amber.korte@student.uva.nl>
2024-01-30 11:49:26 +0100

    docstrings allemaal toegevoegd breadth first

--- code/algorithms/breadthfirst.py
+++ code/algorithms/breadthfirst.py
@@ -4,38 +4,51 @@ import copy
 
 class BreadthFirst(Algorithm):
     '''
-    Initialize Breadthfirst algorithm with deep copy of initial board state.
+    Initialize Breadthfirst algorithm with deep copy of initial board state
 
-    Input:
-    - board = the initial state of Rush Hour board.
+    Attributes
+    ----------
+    board : obj
+        the initial state of a Rush Hour board
+    states : list
+        a queue to store states you still need to look into
+    archive : set
+        keeps track of the states which are already visited
+    visited_states : int
+        amount of states the algorithm did look at to find a solution
     '''
 
     def __init__(self, board: Board):
+        '''
+        Parameters
+        ----------
+        board : obj
+            the initial state of a Rush Hour board
+        '''
         self.board = copy.deepcopy(board)
         
-        # Using queue to store states you still need to look into
         self.states = [copy.deepcopy(self.board)]
         
-        # Keep track of the states which are already visited
         self.archive = set()
         
         self.visited_states = 0
     
     def get_next_state(self):
         '''
-        Get next state through a dequeue operation on the list of states.
+        Get next state through a dequeue operation on the list of states
         
-        Output:
-        - The next state of the board.
+        Output
+        ------
+        board : obj
+            the next state of the board
         '''
         return self.states.pop(0)
 
-    def build_children(self):
+    def possible_states(self):
         '''
-        Get possible states from current board state and add to list if state is not visited before.
+        Get possible states from current board state and add to list if state is not visited before
         '''
         
-        # get possible states from current board state
         possible_states = self.board.get_states()
 
         for state in possible_states:
@@ -46,38 +59,38 @@ class BreadthFirst(Algorithm):
 
     def run(self):
         '''
-        Run the BreadthFirst algorithm until all possible states are visited.
+        Run the BreadthFirst algorithm until all possible states are visited
         
-        Output:
-        - Final state of Rush Hour board.
-        - A boolean indicating if a solution is found.
+        Output
+        ------
+        board : obj
+            final state of Rush Hour board
+        boolean or None : 
+            - True : indicating a solution is found
+            - False : indicating no solution is found yet
+            - None: : no solution is found at all
         '''
-        # check if there are (still) states to explore
-        if self.states != 0:
-            
-            # get next state through dequeue
-            self.board = self.get_next_state()
-            self.visited_states += 1
-            
-            # check if current board state is a solution
-            if self.board.solve():
-                print(f'A solution is found, amount of states visited: {self.visited_states}.')
-                return self.board, True
-            
-            # get possible states for to explore further
-            self.build_children()
-            
-            return self.board, False
-        
-        else:
+        if self.states == 0:
             print(f'No solution is found, amount of states visited: {self.visited_states}.')
             return self.board, None
-    
+        
+        self.board = self.get_next_state()
+        self.visited_states += 1
+        
+        if self.board.solve():
+            print(f'A solution is found, amount of states visited: {self.visited_states}.')
+            return self.board, True
+        
+        self.possible_states()
+        
+        return self.board, False
+        
     def get_name(self):
         '''
-        Get name of the algorithm to save CSV-file.
+        Get name of the algorithm to save CSV-file
         
-        Output:
-        - Name of the algorithm in string.
+        Output
+        ------
+        str : name of the algorithm
         '''
         return f'BreadthFirst_Archive{True}_VisitedStates{self.visited_states}'
