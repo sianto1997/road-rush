4eb5bf63ee748e6aac61ccc547659589830c6d92
Esmeedr <esmeederoo@upcmail.nl>
2024-01-31 14:30:04 +0100

    vizualise

---
+++ code/visualisation/visualize_results.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+df = pd.read_csv('algoritmes_data.csv')
+
+df_melted = pd.melt(df, id_vars=['Algorithm', 'Board'], value_vars=['Moves', 'States'], var_name='Results', value_name='Count')
+
+colors = {'Moves': 'orange', 'States': 'red'}
+sns.barplot(data=df_melted, x='Algorithm', y='Count', hue='Results', log=True, palette=colors)
+plt.title('Results 6x6')
+plt.show()
+
+plt.savefig('plot_results_all_alg')
+
+
+
