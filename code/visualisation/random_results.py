776b95eb27b4d9e2eccfb4910cf96ffaec706ec5
Esmeedr <esmeederoo@upcmail.nl>
2024-01-31 16:06:36 +0100

    extra plots

--- code/visualisation/random_results.py
+++ code/visualisation/random_results.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-def hist_plot(input_file, threshold, output_name, bar_point):
+def hist_plot(input_file, threshold: int, output_name: str, bar_point: int, title: str, width: int):
 
     df = pd.read_csv(input_file)
 
@@ -11,9 +11,10 @@ def hist_plot(input_file, threshold, output_name, bar_point):
     df_other_moves = df_sorted[df_sorted['move'] > threshold]
 
     moves_above_threshold = len(df_other_moves)
+    print(moves_above_threshold)
 
     plt.hist(df_wanted_moves['move'], bins=50, label='Moves up to threshold', rwidth= 0.7)
-    plt.bar(bar_point, moves_above_threshold, width=20, log=True)
+    plt.bar(bar_point, moves_above_threshold, width=width, log=True)
 
     mean = df['move'].mean()
 
@@ -21,10 +22,13 @@ def hist_plot(input_file, threshold, output_name, bar_point):
     plt.xlim(left=0,right=threshold+100)
     plt.xlabel("Amount of moves")
     plt.ylabel("Count")
-    plt.title("Random Experiment results 6x6")
+    plt.title(title)
 
     plt.savefig(output_name)
 
 
-random6x6 = hist_plot('Rushhour6x6_1.csv_RandomAll_random_experiments_S2024-01-23_21-26-02_E2024-01-24_14-05-04.csv', 1000, 'random_6x6.png', 1020)
-random9x9 = hist_plot('Rushhour9x9_4.csv_RandomAll_random_experiments_S2024-01-23_21-26-02_E2024-01-24_14-05-04.csv', 5000,'random_9x9')
+random6x6 = hist_plot('Rushhour6x6_1.csv_RandomAll_random_experiments_S2024-01-23_21-26-02_E2024-01-24_14-05-04.csv', 1000, 'random_6x6.png', 1020,"Random Experiment results 6x6", 20)
+plt.show()
+random9x9 = hist_plot('Rushhour9x9_4.csv_RandomAll_random_experiments_S2024-01-23_21-26-02_E2024-01-24_14-05-04.csv', 5000,'random_9x9', 5020, 'Random Experiment results 9x9', 40)
+plt.show()
+random12x12 = hist_plot('Rushhour12x12_7.csv_RandomAll_random_experiments_S2024-01-24_16-26-23_E2024-01-26_03-53-12.csv', 10000, 'random_12x12.png', 10020,'Random Experiment results 12x12', 60)
